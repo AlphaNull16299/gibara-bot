0a02ac4da162bc48e3af20fdd02dd95f78572e4d
AlphaNull <58626192+AlphaNull16299@users.noreply.github.com>
2021-01-31 09:30:32 +0900

    Update run.py

--- run.py
+++ run.py
@@ -33,30 +33,32 @@ class MyBot(commands.Bot):
         return await self.change_presence(status=discord.Status.do_not_disturb, activity=activity)
 
   
+    @tasks.loop(seconds=1.0)
+     async def wait_for_tao(self):
+         if int(time()) >= self.now_time + 10:
+             await self.get_channel(c_id).send("起動か再起動しました")
+             self.now_time = int(time())
+
      async def on_message(self, message):
-          user_id = message.author.id
-          if message.content.startswith(prefix):
-              return await self.process_commands(message)
+         user_id = message.author.id
+         if message.content.startswith(prefix):
+             return await self.process_commands(message)
 
-          if message.channel.id == c_id:
-              if user_id == 695288604829941781 and message.embeds and f"{self.user.mention}さん...\nゲームにログインしてね！！\n[コマンドは::loginだよ！！]" == message.embeds[0].description:
-                  await asyncio.sleep(10)
-                  await message.channel.send("::login")
-                  await asyncio.sleep(10)
-                  await message.channel.send("::t")
-                  self.now_time = int(time)
+         if message.channel.id == c_id:
+             if user_id == 695288604829941781 and message.embeds and f"{self.user.mention}さん...\nゲームにログインしてね！！\n[コマンドは::loginだよ！！]" == message.embeds[0].description:
+                 await asyncio.sleep(10)
+                 await message.channel.send("::login")
+                 await asyncio.sleep(10)
+                 await message.channel.send("::t")
+                 self.now_time = int(time)
 
-              if user_id == 664790025040429057:
-                  await asyncio.sleep(1)
-                  await message.channel.send(message.content)
-                  await self.wait_for("message_edit", check=lambda b, a: a.channel.id == c_id and a.author.id == 695288604829941781)
-                  await asyncio.sleep(3)
-                  await message.channel.send("::t")
-                  self.now_time = int(time)
-
-
-
-
+             if user_id == 664790025040429057:
+                 await asyncio.sleep(1)
+                 await message.channel.send(message.content)
+                 await self.wait_for("message_edit", check=lambda b, a: a.channel.id == c_id and a.author.id == 695288604829941781)
+                 await asyncio.sleep(3)
+                 await message.channel.send("::t")
+                 self.now_time = int(time)
 
    async def on_message(self, message):
          user_id = message.author.id
